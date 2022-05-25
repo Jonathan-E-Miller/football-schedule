8beaf0b8f88f68dde21550bd20f8ddef651462d9
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2022-05-25 23:39:45 +0100

    Implement Round Robin algorithm and write basic unit test
    This commit closes #1, closes #2, closes #4 and closes #5

---
+++ FixtureGenerator/FixtureGenerator/FixtureGenerator.cs
@@ -0,0 +1,81 @@
+﻿namespace FixtureGenerator
+{
+    public class FixtureGenerator
+    {
+        public enum EAlgorithmOption
+        {
+            eRoundRobin = 0,
+            eBackTrack = 1
+        };
+
+        public FixtureGenerator()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fixtureEntities"></param>
+        /// <param name="option"></param>
+        /// <param name="constraint"></param>
+        /// <returns></returns>
+        public IEnumerable<IEnumerable<IFixture>> GenerateFixtureList(
+            IEnumerable<IFixtureEntity> fixtureEntities, 
+            EAlgorithmOption option = EAlgorithmOption.eRoundRobin, 
+            List<Func<bool>>? constraint = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate all rounds of fixtures using the round robin algorithm
+        /// </summary>
+        /// <typeparam name="M">The "match" object that implements IFixture interface</typeparam>
+        /// <typeparam name="T">The "team" object that implements IFixtureEntity interface</typeparam>
+        /// <param name="fixtureEntities">A list of teams or players to organise into a set of rounds</param>
+        /// <returns>A list of lists, the inner list represents a round of matches.</returns>
+        public List<List<M>> RoundRobin<M,T>(IEnumerable<T> fixtureEntities) where M : IFixture, new() where T : IFixtureEntity
+        {
+            List<List<M>> fixtureList = new List<List<M>>();
+
+            int numberOfRounds = fixtureEntities.Count() - 1;
+            int numberOfMatchesPerRound = fixtureEntities.Count() / 2;
+
+            List<T> roundRobin = new List<T>();
+            roundRobin.AddRange(fixtureEntities.Skip(1));
+
+            int numberOfTeamsInRound = roundRobin.Count();
+            for (int round = 0; round < numberOfRounds; round++)
+            {
+                List<M> matchesInRound = new List<M>();
+                T firstTeam = fixtureEntities.ElementAt(0);
+                T awayTeam = roundRobin.ElementAt((round + numberOfTeamsInRound - 1) % numberOfTeamsInRound);
+
+                M match = new M();
+                match.HomeEntity = firstTeam;
+                match.AwayEntity = awayTeam;
+                matchesInRound.Add(match);
+
+                for (int i = 0; i < numberOfMatchesPerRound - 1; i++)
+                {
+                    M roundMatch = new M();
+                    int homeIdx = (i + round) % numberOfTeamsInRound;
+                    int awayIdx = (i + round + numberOfTeamsInRound - 2) % numberOfTeamsInRound;
+
+                    roundMatch.HomeEntity = roundRobin.ElementAt(homeIdx);
+                    roundMatch.AwayEntity = roundRobin.ElementAt(awayIdx);
+
+                    matchesInRound.Add(roundMatch);
+                }
+                fixtureList.Add(matchesInRound);
+            }
+
+            return fixtureList;
+        }
+
+        private List<List<IFixture>> BackTrack(IEnumerable<IFixtureEntity> fixtureEntities, List<Func<bool>>? constraints = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FixtureGenerator/FixtureGeneratorTests/Framework/Team.cs
@@ -0,0 +1,19 @@
+﻿using FixtureGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixtureGeneratorTests.Framework
+{
+    public class Team : IFixtureEntity
+    {
+        public string Code { get; set; }
+
+        public Team(string code)
+        {
+            Code = code;
+        }
+    }
+}

4463bdca8b5bf2e2ce6d58b7c21650d64c28d12b
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2024-01-21 16:15:36 +0000

    Tidying up

--- FixtureGenerator/FixtureGenerator/IFixtureVerifier.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FixtureGenerator
-{
-    public interface IFixtureVerifier
-    {
-        public void VerifySecondHalf<M>(List<List<M>> firstHalf, List<List<M>> secondHalf) where M : IFixture;
-    }
-}

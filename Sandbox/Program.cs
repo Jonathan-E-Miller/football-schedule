087f92f4169e640f8ac269d3ca807f4f9a7646c7
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2024-01-21 21:08:44 +0000

    Adding sandbox app
    Modified build_and_test.yml to exclude the Sandbox project from coverage.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sandbox;
+using System.Numerics;
+using System;
+using FixtureGenerator;
+
+Console.WriteLine("Fixture Generator");
+
+var teams = new List<Team>()
+{
+    new("Arsenal"),
+    new("Aston Villa"),
+    new("Bournemouth"),
+    new("Brentford"),
+    new("Brighton"),
+    new("Burnley"),
+    new("Chelsea"),
+    new("Crystal Palace"),
+    new("Everton"),
+    new("Fulham"),
+    new("Liverpool"),
+    new("Luton Town"),
+    new("Man. City"),
+    new("Manchester Utd"),
+    new("Newcastle"),
+    new("Nottingham"),
+    new("Sheffield Utd"),
+    new("Tottenham"),
+    new("West Ham"),
+    new("Wolves")
+};
+
+teams.Shuffle();
+var season = new Generator().GenerateFixtures<Fixture, Team>(teams);
+
+var week = 1;
+foreach (var round in season)
+{
+    Console.WriteLine($"Week {week++}");
+    round.ForEach(x => Console.WriteLine(x.ToString()));
+    Console.WriteLine(Environment.NewLine);
+}    
---
+++ Sandbox/Team.cs
@@ -0,0 +1,20 @@
+﻿using FixtureGenerator;
+
+namespace Sandbox
+{
+    public class Team : IFixtureEntity
+    {
+        private readonly string _name;
+        public Team(string name)
+        {
+            _name = name;
+        }
+
+        public string Code => _name;
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
